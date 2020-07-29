1b37c5de0ce1cb0cb94f9a3ee59f8fbfd392c905
kamilwowel11 <kamilr1998@o2.pl>
2020-07-29 12:54:21 +0200

    Starting project with simple loop and graphics

---
+++ src/main/Player.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.awt.*;
+
+public class Player extends GameObject{
+
+    public Player(int x, int y,ID id){
+        super(x,y,id);
+
+    }
+
+
+    public void tick() {
+        x += velX;
+        y += velY;
+
+    }
+
+
+    public void render(Graphics g) {
+        if (id == ID.Player) g.setColor(Color.white);
+        if (id == ID.Player2) g.setColor(Color.blue);
+        g.fillRect(x,y,32,32);
+
+    }
+}
